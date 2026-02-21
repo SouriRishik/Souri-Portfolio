037da9ceecada3a993242bc410ba3a0bb4a6ce85
SouriRishik <souri.rishik27@gmail.com>
2026-02-21 20:10:41 +0530

    feat: add scroll functionality to HeroSection button

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -171,7 +171,11 @@ export default function HeroSection() {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ delay: 1.5 }}
-        className="absolute bottom-8 left-1/2 -translate-x-1/2"
+        className="absolute bottom-8 left-1/2 -translate-x-1/2 cursor-pointer"
+        onClick={() => {
+          document.getElementById('about')?.scrollIntoView({ behavior: 'smooth' });
+        }}
+        title="Scroll to About"
       >
         <motion.div
           animate={{ y: [0, 8, 0] }}
